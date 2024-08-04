e27e934809fc09602de1536b410ffb6ee047a58a
Xpiee <anuhavbhatti@yahoo.com>
2024-08-04 04:28:54 +0000

    dataLoader class for SegClsNet

--- nnunetv2/training/dataloading/base_data_loader.py
+++ nnunetv2/training/dataloading/base_data_loader.py
@@ -25,7 +25,7 @@ class nnUNetDataLoaderBase(DataLoader):
         self.oversample_foreground_percent = oversample_foreground_percent
         self.final_patch_size = final_patch_size
         self.patch_size = patch_size
-        self.list_of_keys = list(self._data.keys())
+        self.list_of_keys = list(self._data.keys()) 
         # need_to_pad denotes by how much we need to pad the data so that if we sample a patch of size final_patch_size
         # (which is what the network will get) these patches will also cover the border of the images
         self.need_to_pad = (np.array(patch_size) - np.array(final_patch_size)).astype(int)
---
+++ nnunetv2/training/dataloading/data_loader_segcls_3d.py
@@ -0,0 +1,108 @@
+import numpy as np
+import torch
+from threadpoolctl import threadpool_limits
+from typing import Union, Tuple, List
+
+from nnunetv2.training.dataloading.base_data_loader import nnUNetDataLoaderBase
+from nnunetv2.training.dataloading.nnunet_dataset import nnUNetDataset
+from nnunetv2.utilities.label_handling.label_handling import LabelManager
+
+class nnUNetSegClsDataLoader3D(nnUNetDataLoaderBase):
+    def __init__(self,
+                 data: nnUNetDataset,
+                 batch_size: int,
+                 patch_size: Union[List[int], Tuple[int, ...], np.ndarray],
+                 final_patch_size: Union[List[int], Tuple[int, ...], np.ndarray],
+                 label_manager: LabelManager,
+                 oversample_foreground_percent: float = 0.0,
+                 sampling_probabilities: Union[List[int], Tuple[int, ...], np.ndarray] = None,
+                 pad_sides: Union[List[int], Tuple[int, ...], np.ndarray] = None,
+                 probabilistic_oversampling: bool = False,
+                 transforms=None):
+        super().__init__(data, batch_size, patch_size, final_patch_size, label_manager, oversample_foreground_percent,
+                         sampling_probabilities, pad_sides, probabilistic_oversampling, transforms)
+        self.data_shape, self.seg_shape = self.determine_shapes()
+        self.cls_shape = self.determine_cls_shape()
+
+
+    def determine_shapes(self):
+        # load one case
+        data, seg, _, properties = self._data.load_case(self.indices[0])
+        num_color_channels = data.shape[0]
+
+        data_shape = (self.batch_size, num_color_channels, *self.patch_size)
+        seg_shape = (self.batch_size, seg.shape[0], *self.patch_size)
+        cls_shape = (self.batch_size,)
+        return data_shape, seg_shape
+
+    def determine_cls_shape(self):
+        # Classification label shape
+        return (self.batch_size,)
+
+    def generate_train_batch(self):
+        selected_keys = self.get_indices()
+        # preallocate memory for data, seg, and classification labels
+        data_all = np.zeros(self.data_shape, dtype=np.float32)
+        seg_all = np.zeros(self.seg_shape, dtype=np.int16)
+        cls_all = np.zeros(self.cls_shape, dtype=np.int64)
+        case_properties = []
+
+        for j, i in enumerate(selected_keys):
+            # oversampling foreground will improve stability of model training, especially if many patches are empty
+            # (Lung for example)
+            force_fg = self.get_do_oversample(j)
+
+            data, seg, classification_label, properties = self._data.load_case(i)
+            case_properties.append(properties)
+
+            # If we are doing the cascade then the segmentation from the previous stage will already have been loaded by
+            # self._data.load_case(i) (see nnUNetDataset.load_case)
+            shape = data.shape[1:]
+            dim = len(shape)
+            bbox_lbs, bbox_ubs = self.get_bbox(shape, force_fg, properties['class_locations'])
+
+            # whoever wrote this knew what he was doing (hint: it was me). We first crop the data to the region of the
+            # bbox that actually lies within the data. This will result in a smaller array which is then faster to pad.
+            # valid_bbox is just the coord that lied within the data cube. It will be padded to match the patch size
+            # later
+            valid_bbox_lbs = np.clip(bbox_lbs, a_min=0, a_max=None)
+            valid_bbox_ubs = np.minimum(shape, bbox_ubs)
+
+            # At this point you might ask yourself why we would treat seg differently from seg_from_previous_stage.
+            # Why not just concatenate them here and forget about the if statements? Well that's because seg needs to
+            # be padded with -1 constant whereas seg_from_previous_stage needs to be padded with 0s (we could also
+            # remove label -1 in the data augmentation but this way it is less error prone)
+            this_slice = tuple([slice(0, data.shape[0])] + [slice(i, j) for i, j in zip(valid_bbox_lbs, valid_bbox_ubs)])
+            data = data[this_slice]
+
+            this_slice = tuple([slice(0, seg.shape[0])] + [slice(i, j) for i, j in zip(valid_bbox_lbs, valid_bbox_ubs)])
+            seg = seg[this_slice]
+
+            padding = [(-min(0, bbox_lbs[i]), max(bbox_ubs[i] - shape[i], 0)) for i in range(dim)]
+            padding = ((0, 0), *padding)
+            data_all[j] = np.pad(data, padding, 'constant', constant_values=0)
+            seg_all[j] = np.pad(seg, padding, 'constant', constant_values=-1)
+            cls_all[j] = classification_label
+
+        if self.transforms is not None:
+            with torch.no_grad():
+                with threadpool_limits(limits=1, user_api=None):
+                    data_all = torch.from_numpy(data_all).float()
+                    seg_all = torch.from_numpy(seg_all).to(torch.int16)
+                    cls_all = torch.from_numpy(cls_all).long()
+                    images = []
+                    segs = []
+                    for b in range(self.batch_size):
+                        tmp = self.transforms(**{'image': data_all[b], 'segmentation': seg_all[b]})
+                        images.append(tmp['image'])
+                        segs.append(tmp['segmentation'])
+                    data_all = torch.stack(images)
+                    if isinstance(segs[0], list):
+                        seg_all = [torch.stack([s[i] for s in segs]) for i in range(len(segs[0]))]
+                    else:
+                        seg_all = torch.stack(segs)
+                    del segs, images
+
+            return {'data': data_all, 'target': seg_all, 'cls_target': cls_all, 'keys': selected_keys}
+
+        return {'data': data_all, 'target': seg_all, 'cls_target': cls_all, 'keys': selected_keys}
